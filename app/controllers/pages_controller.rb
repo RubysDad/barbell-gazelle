fb5dc47f49cee4ac10d78fc0a19161d4f280d813
Rubysdad <jordan@devcamp.com>
2017-08-03 18:11:59 +0000

    fininshed instagram implementation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   layout "pages"
   
   def index
-    @instagram = Instagram.user_recent_media("5665620823", {:count => 8})
+    @instagram = Instagram.user_recent_media("5665620823", {:count => 4})
   end
 end
